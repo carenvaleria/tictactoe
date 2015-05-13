d41a2a2ca944f8bb5c49e70707916735466b2471
Caren Garcia <carenvaleria90@gmail.com>
2015-05-13 15:05:55 -0500

    Adding bootstrap

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//setting up the board
+$(document).ready(function() {
+  var x = "&ouml;"
+  var o = "&oslash;"
+  var count = 0;
+  var o_win = 0;
+  var x_win = 0;
+
+$('').click(function(){
+
+
+
