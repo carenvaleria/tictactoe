020e620a91fed178e07ddf881a7a9a28a08bcb4f
Caren Garcia <carenvaleria90@gmail.com>
2015-05-13 23:22:25 -0500

    Cleaned up some of the CSS and JS

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
 //setting up the board
 $(document).ready(function() {
-  var x = "&ouml;"
-  var o = "&oslash;"
-  var count = 0;
-  var o_win = 0;
-  var x_win = 0;
-
-$('').click(function(){
-
+	$('.button').click(function(){
+		$(this).append("&ouml; or&oslash;");
+		console.log("button clicked");
+	});
+});
 
 
