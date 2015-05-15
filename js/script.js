bc787a1be812d3470580c314c9a02744b63c51d2
Caren Garcia <carenvaleria90@gmail.com>
2015-05-15 10:01:50 -0500

    added reset button and some functionality to js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,35 @@
 //setting of the board
-// var grid =  [[0,0,0],[0,0,0],[0,0,0]];
-// var player1 = 0;
-// var	player2 = 0;
-// var getWinner = 0, moveCount=0;
+var grid = [[0,0,0],[0,0,0],[0,0,0]];
+var player1 = "images/oslash.png";
+var	player2 = "images/ouml.png";
+var currentPlayer = "images/oslash.png"
+
 
 //function for player input moves
 $(document).ready(function(){
-	var currentPlayer = "images/ouml.png"
+	var currentPlayer = "images/oslash.png"
 	 
 	$('.button').click(function (){
 	
 	     $(this).children().attr("src", currentPlayer);
-	     if(currentPlayer == "images/ouml.png") {
-			currentPlayer = "images/oslash.png"; 
+	     if(currentPlayer == "images/oslash.png") {
+			currentPlayer = "images/ouml.png"; 
 			} 
 
-			else if(currentPlayer == "images/oslash.png") {
-			currentPlayer = "images/ouml.png"; 
+			else if(currentPlayer == "images/ouml.png") {
+			currentPlayer = "images/oslash.png"; 
 			}  
 	});
 });
-
 //check for winner
+function getWinner(){
+	if(winnerIsOslash()){
+		return 'oslash';
+	}if(winnerIsOuml()){
+		return '&ouml;';
+	}
+	return null;
+}
 
-// function getWinner(){
 
-// }
+
