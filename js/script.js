8f6e8738f26fb64b5014ecaed3b6f97e2931e217
Caren Garcia <carenvaleria90@gmail.com>
2015-05-14 19:31:21 -0500

    have a functioning click button with images working

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
 //setting of the board
-var grid =  [[0,0,0],[0,0,0],[0,0,0]];
-var getWinner = 0, moveCount=0;
-
-function boardMsg(x){
-    return $("#board").text(x);
-}
+// var grid =  [[0,0,0],[0,0,0],[0,0,0]];
+// var player1 = 0;
+// var	player2 = 0;
+// var getWinner = 0, moveCount=0;
 
 //function for player input moves
-$(".button").click(function (){
-    var row = $(this).parent().index();
-    var col = $(this).children();
+$(document).ready(function(){
+	var currentPlayer = "images/ouml.png"
+	 
+	$('.button').click(function (){
+	
+	     $(this).children().attr("src", currentPlayer);
+	     if(currentPlayer == "images/ouml.png") {
+			currentPlayer = "images/oslash.png"; 
+			} 
+
+			else if(currentPlayer == "images/oslash.png") {
+			currentPlayer = "images/ouml.png"; 
+			}  
+	});
+});
 
 //check for winner
 
-function getWinner(){
+// function getWinner(){
 
-}
+// }
