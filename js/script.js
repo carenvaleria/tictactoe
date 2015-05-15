a6a3bc3cc6793f9476f1366bd3ab628a08590d32
Caren Garcia <carenvaleria90@gmail.com>
2015-05-15 13:46:11 -0500

    set up switch players and proper set up of board

--- js/script.js
+++ js/script.js
@@ -1,35 +1,63 @@
 //setting of the board
-var grid = [[0,0,0],[0,0,0],[0,0,0]];
 var player1 = "images/oslash.png";
 var	player2 = "images/ouml.png";
-var currentPlayer = "images/oslash.png"
+var player1Class = "oslash";
+var player2Class = "ouml";
+var currentPlayer = player1;
 
 
 //function for player input moves
 $(document).ready(function(){
-	var currentPlayer = "images/oslash.png"
 	 
 	$('.button').click(function (){
-	
-	     $(this).children().attr("src", currentPlayer);
-	     if(currentPlayer == "images/oslash.png") {
-			currentPlayer = "images/ouml.png"; 
-			} 
+		console.log(currentPlayer);
+	    $(this).children().attr("src", currentPlayer);
+	    $(this).children().show();
+	    if(currentPlayer == player1) {
+	    	$(this).addClass(player1Class);
+	 
+		} 
+		else if(currentPlayer == player2) {
+			$(this).addClass(player2Class);
 
-			else if(currentPlayer == "images/ouml.png") {
-			currentPlayer = "images/oslash.png"; 
-			}  
+		} 
+
+	    getWinner();
+
+	    if(currentPlayer == player1) {
+			currentPlayer = player2; 
+		} 
+
+		else if(currentPlayer == player2) {
+			currentPlayer = player1; 
+		}  
+
+		
 	});
 });
-//check for winner
-function getWinner(){
-	if(winnerIsOslash()){
-		return 'oslash';
-	}if(winnerIsOuml()){
-		return '&ouml;';
-	}
-	return null;
+
+//check wins on rows
+checkWin($('#a'), $('#b'), $('#c'));
+checkWin($('#d'), $('#e'), $('#g'));
+checkWin($('#g'), $('#h'), $('#i'));
+
+//check wins on columns
+checkWin($('#a'), $('#d'), $('#g'));
+checkWin($('#b'), $('#e'), $('#h'));
+checkWin($('#c'), $('#g'), $('#i'));
+
+//check wins diagonally
+checkWin($('#a'), $('#e'), $('#g'));
+checkWin($('#g'), $('#e'), $('#c'));
+
+//get winner
+function getWinner() {
+
+//$(elem)hasClass(x), ()
+
 }
+
+
 
 
 
